a9fc456fd35efe78c2d33bd139da382f2f722c6d
dalongrong <1141591465@qq.com>
2018-04-13 10:51:47 +0800

    init project

---
+++ README.md
@@ -0,0 +1,21 @@
+# web develop with gopherjs
+
+## how to run
+
+* install gopherjs
+
+```bash
+go get -u github.com/gopherjs/gopherjs
+```
+
+* build
+
+```golang
+gopherjs build app.go
+```
+
+* local webserevr start
+
+```nodejs
+yarn && yarn run start
+```
---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"time"
+)
+
+func term(k float64) float64 {
+	return 4 * math.Pow(-1, k) / (2*k + 1)
+}
+
+// pi performs n iterations to compute an approximation of pi using math.Pow.
+func pi(n int32) float64 {
+	f := 0.0
+	for k := int32(0); k <= n; k++ {
+		f += term(float64(k))
+	}
+	return f
+}
+
+func main() {
+	// Start measuring time from now.
+	started := time.Now()
+
+	const n = 50 * 1000 * 1000
+	fmt.Printf("approximating pi with %v iterations.\n", n)
+	fmt.Println(pi(n))
+
+	fmt.Println("total time taken is:", time.Since(started))
+}
